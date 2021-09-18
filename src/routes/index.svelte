a0d1ecfd1b705e16095aad5e961d58c91dc8208f
fernaned <fernandoed.bm@gmail.com>
2021-09-17 23:54:43 -0500

    crud

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,54 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
-
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	import { onMount } from 'svelte';
+
+	interface Diplomado {
+		id: Number;
+		nombre: string;
+	}
+
+	let diplomados: Diplomado[] = [];
+	let text;
+
+	const serverURL = 'http://localhost:8000';
+
+	onMount(async () => {
+		let res = await fetch(`${serverURL}/diplomados`);
+		let data = await res.json();
+		diplomados = data;
+	});
+
+	const handleSubmit = async () => {
+		let res = await fetch(`${serverURL}/diplomados`, {
+			method: 'POST',
+			body: JSON.stringify({ nombre: text }),
+			headers: {
+				'Content-type': 'application/json'
+			}
+		});
+		let data = await res.json();
+		console.log(data, text);
+	};
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
+<div>
+	<form action="" on:submit|preventDefault={handleSubmit}>
+		<input type="text" bind:value={text} />
+		<button type="submit">Add diplomado</button>
+	</form>
+
+	{#each diplomados as diplomado (diplomado.id)}
+		<div>
+			<p>{diplomado.nombre}</p>
 		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+	{/each}
+</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	input {
+		border: 1px solid #000000dd;
 	}
 </style>
