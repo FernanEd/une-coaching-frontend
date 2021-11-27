32d99c12e422eb87fc01e3d6f516e5784914fe31
fernaned <fernandoed.bm@gmail.com>
2021-11-26 19:21:25 -0600

    testing onload loading dependencies once

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -30,37 +30,13 @@
 			if (!session.roles.includes(portal)) return goIndex;
 		}
 
-		//Make the call
-		db_usuarios.getItems();
-		db_docentes.getItems();
-		db_coaches.getItems();
-		db_instructores.getItems();
-		db_administrativos.getItems();
-		db_coordinadores.getItems();
-
-		db_docentesEnCoaches.getItems();
-
-		db_jornadas.getItems();
-		db_cursosEnJornada.getItems();
-		db_asistentesEnCurso.getItems();
-		db_reportes.getItems();
-
-		db_registrosCompetencias.getItems();
-
-		db_registrosCursos.getItems();
-		db_registrosDiplomados.getItems();
-
-		db_cursos.getItems();
-		db_diplomados.getItems();
-		db_competencias.getItems();
-		db_tiposCompetencias.getItems();
-
 		return goThrough;
 	}
 </script>
 
 <script>
 	import { session } from '$app/stores';
+	import LoadingSpinner from '$lib/components/common/loadingSpinner.svelte';
 	import PromptArea from '$lib/components/common/promptArea.svelte';
 	import ToastArea from '$lib/components/common/toastArea.svelte';
 	import {
@@ -83,11 +59,59 @@
 		db_tiposCompetencias,
 		db_usuarios,
 	} from '$lib/stores/db';
+	import { onMount } from 'svelte';
 	import '../app.postcss';
+
+	export let loading = true;
+
+	onMount(async () => {
+		db_usuarios.getItems();
+		db_docentes.getItems();
+		db_coaches.getItems();
+		db_instructores.getItems();
+		db_administrativos.getItems();
+		db_coordinadores.getItems();
+		db_docentesEnCoaches.getItems();
+		db_jornadas.getItems();
+		db_cursosEnJornada.getItems();
+		db_asistentesEnCurso.getItems();
+		db_reportes.getItems();
+		db_registrosCompetencias.getItems();
+		db_registrosCursos.getItems();
+		db_registrosDiplomados.getItems();
+		db_cursos.getItems();
+		db_diplomados.getItems();
+		db_competencias.getItems();
+		db_tiposCompetencias.getItems();
+
+		let miPrimeraPromise = new Promise((resolve, reject) => {
+			// Llamamos a resolve(...) cuando lo que estabamos haciendo finaliza con éxito, y reject(...) cuando falla.
+			// En este ejemplo, usamos setTimeout(...) para simular código asíncrono.
+			// En la vida real, probablemente uses algo como XHR o una API HTML5.
+			setTimeout(function () {
+				resolve('¡Éxito!'); // ¡Todo salió bien!
+			}, 2000);
+		});
+
+		console.log('ye');
+
+		await miPrimeraPromise;
+
+		loading = false;
+	});
 </script>
 
 {#if $session.isLoggedIn}
-	<slot />
+	{#if loading}
+		<div
+			class="w-screen h-screen flex justify-center items-center flex-col gap-8"
+		>
+			<p class="text-accent">Cargando el portal...</p>
+			<LoadingSpinner />
+		</div>
+	{:else}
+		<slot />
+	{/if}
 {:else}
 	<slot name="login" />
 {/if}
